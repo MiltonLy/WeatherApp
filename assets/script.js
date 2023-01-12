84e70be83cd3d0e2d2e8df12bdd31f76f2e91971
Milton Ly <ly.milton@gmail.com>
2023-01-11 23:47:54 -0800

    weather working

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,27 @@
+let weatherAPIKey = 'eea5b144b409aceaec1caf47854c2562';
+// let weatherBasePoint = 'https://api.openweathermap.org/data/2.5/weather?appid='
+
+// let weatherByCity = weatherBasePoint =  async (city) => {
+//     let endpoint = weatherBasePoint + '&q=' + city;
+//     let response = await fetch(endpoint);
+//     console.log(response);
+// }
+
+// weatherByCity('San Francisco');
 
 class Fetch {
     async getCurrent(input){
-            const response = await fetch(
-             `https://openweathermap.org/data/2.5/forecast?lat=${input}&appid=eea5b144b409aceaec1caf47854c2562`
-            )
-            const data = await response.json();
-            console.log(data);
-            return data;
+            let response = await 
+                
+                fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${input}&limit=5&appid=eea5b144b409aceaec1caf47854c2562`)
+                .then(r=> r.json())
+                .then(data => {return data})
+            return await fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${response[0].lat}&lon=${response[0].lon}&appid=` + weatherAPIKey + '&units=imperial')
+            .then(response => response.json())
+            .then(data => {return data})
     }   
 }
+
 
 class UI {
     constructor(){
@@ -17,19 +30,20 @@ class UI {
         this.defaultCity= "San Francisco";
     }
 
-    populateContainer(data) {
-        this.infoContainer.innerHTML = `
-            <div class = "card-body justify-content-center">
-                <h2 class="card-title">${data.name}</h2>
-                <h3 class="card-subtitle"> Temp: ${data.main.temp}</h3>
-                <h3 class="card-subtitle"> Wind: ${datat.main.wind}</h3>
-                <h3 class="card-subtitle"> Condition: ${data.weather[0].description}</3>
+    populateContainer(data, cityName) {
+        this.infoContainer.innerHTML += `
+            <div class = "card-info justify-content-center">
+                <h2 class="card-title">${cityName}</h2>
+                <h5 class="card-subtitle"> Date: ${data.dt_txt}</h5>
+                <h5 class="card-subtitle"> Temp: ${data.main.temp}</h5>
+                <h5 class="card-subtitle"> Wind: ${data.wind.speed}</h5>
+                <h5 class="card-subtitle"> Condition: ${data.weather[0].description}</5>
             </div>
         `;
     }
 
     clearContainer(){
-        infoContainer.innerHTML = "";
+        this.infoContainer.innerHTML = "";
     }
 
     saveToLocalStorage(){
@@ -56,15 +70,18 @@ const search = document.getElementById("searchUser");
 const buttonEl = document.getElementById("btn");
 
 buttonEl.addEventListener('click', () => {
+   ui.clearContainer();
     const currentValue = search.value;
 
     ft.getCurrent(currentValue).then((data)=>{
-        ui.populateContainer(data);
-        ui.saveToLocalStorage(data);
+        console.log(data)
+        data.list.slice(0,6).forEach(information=>ui.populateContainer(information, data.city.name));
+
+        // ui.saveToLocalStorage(data);
     })
 });
 
 window.addEventListener("DOMContentLoaded", () => {
     const dataSaved = ui.getFromLocalStorage();
-    ui.populateContainer(dataSaved);
+    // ui.populateContainer(dataSaved);
 })
